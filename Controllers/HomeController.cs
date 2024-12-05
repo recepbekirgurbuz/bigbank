3458f5c9b7cdfb2ec0c8dd98980e4b5bab22f16b
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-12-05 19:06:25 +0300

    database connection and model are called and all users are listed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,23 @@
 using System.Diagnostics;
+using bigbank.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bigbank.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BigbankContext _context;
+
+        public HomeController(BigbankContext context)
+        {
+            _context = context;
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
-            return View();
+            var users = _context.Users.ToList();
+            return View(users);
         }
     }
 }
