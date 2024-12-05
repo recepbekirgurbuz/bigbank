bfe69cc1674a9736dc8aad734bfd34f551523a48
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-12-05 19:12:23 +0300

    database connection and model are called and the code required for the login process is written

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using bigbank.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bigbank.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly BigbankContext _context;
+
+        public AccountController(BigbankContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("/loginPost")]
+        public IActionResult LoginPost(string username, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                return Content($"Hoş geldiniz, {user.Name} {user.Surname}!");
+            }
+
+            ModelState.AddModelError(string.Empty, "Kullanıcı adı veya şifre hatalı.");
+            return View();
+        }
+    }
+}
