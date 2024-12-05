67134c310a33a8684b9a6cc547e30ade339ac508
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-12-05 11:32:45 +0300

    Database tables were taken as models using scaffold.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bigbank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bigbank.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+}
