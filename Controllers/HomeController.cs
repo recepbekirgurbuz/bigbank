d1e49d5a7ab6221b83e83127113be5e2fb869e41
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-12-20 23:05:12 +0300

    We showed the data coming from the controller on the screen. If the user is not logged in, we redirected to the /login page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,23 @@ namespace bigbank.Controllers
         {
             var users = _context.Users.ToList();
             // Oturum kontrolü
+            if (HttpContext.Session.GetInt32("UserID") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            ViewBag.Username = HttpContext.Session.GetString("Username");
+            return View();
+        }
+
+        [HttpGet("/Home")]
+        public IActionResult Home()
+        {
+            var users = _context.Users.ToList();
+            var userId = HttpContext.Session.GetInt32("Id");
+            var account = _context.Accounts.FirstOrDefault(t => t.CustomerId == userId);
+            ViewBag.AccountBalance = account.Balance;
+            // Oturum kontrolü
             if (HttpContext.Session.GetInt32("Id") == null)
             {
                 return RedirectToAction("Login", "Account");
