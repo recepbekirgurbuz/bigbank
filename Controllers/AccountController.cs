c63017ba0d0258ed22f710a92b984e5522283e5c
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-12-20 22:58:59 +0300

    We pulled the data of the logged in users and sent them to view

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -24,6 +24,29 @@ namespace bigbank.Controllers
             return View();
         }
 
+        [HttpGet("/details")]
+        public IActionResult Details()
+        {
+            var userId = HttpContext.Session.GetInt32("Id");
+            var user = _context.Users.FirstOrDefault(u => u.UserId == userId);
+            var customer = _context.Customers.FirstOrDefault(c => c.CustomerId == userId);
+            var transaction = _context.Transactions.FirstOrDefault(t => t.TransactionId == userId);
+            var account = _context.Accounts.FirstOrDefault(t => t.CustomerId == userId);
+            var currentPath = HttpContext.Request.Path;
+
+            var formattedDate = account.CreatedDate?.ToString("dd/MM/yyyy");
+            ViewBag.AccountCreatedDate = formattedDate;
+
+            ViewBag.Users = userId;
+            ViewBag.CustomerFullName = customer.Name + " " + customer.Surname;
+            ViewBag.CustomerID = customer.CustomerId;
+            ViewBag.Iban = account.AccountNumber;
+            ViewBag.AccountType = account.AccountType;
+            ViewBag.AccountBalance = account.Balance;
+            
+            return View(user);
+        }
+
         [HttpPost("/loginPost")]
         public IActionResult LoginPost(string username, string password)
         {
@@ -31,11 +54,8 @@ namespace bigbank.Controllers
 
             if (user != null)
             {
-                // Oturum bilgilerini saklama
-                HttpContext.Session.SetInt32("Id", user.Id);
-                HttpContext.Session.SetString("Username", user.Username);
-
-                return RedirectToAction("Index", "Home");
+                HttpContext.Session.SetInt32("Id", user.UserId);
+                return RedirectToAction("Home", "Home");
             }
 
             ModelState.AddModelError(string.Empty, "Kullanıcı adı veya şifre hatalı.");
